ed5cf4619aa1aa5a9faeba5c53679e67eac70690
tpei <t.s.peikert@gmail.com>
2022-03-21 09:22:19 +0100

    update curl command

--- verify.sh
+++ verify.sh
@@ -46,7 +46,7 @@ verify_and_clean()
 if ! [ -x "$(command -v faas-cli)" ]; then
     HERE=`pwd`
     cd /tmp/
-    curl --insecure https://cli.openfaas.com | sh
+    curl -L https://cli.openfaas.com | sh
     CLI="/tmp/faas-cli"
 
     cd $HERE
